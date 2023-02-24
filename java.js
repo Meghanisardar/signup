943ed1a1b0e27f19c1a2aeb0ab572613c2c4c4cb
Meghanisardar <sardarmeghani6@gmail.com>
2023-02-24 09:54:10 -0800

    signup form

---
+++ java.js
@@ -0,0 +1,43 @@
+function signup() {
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  let messege = document.getElementById("messege");
+  if (email.value === "") {
+    messege.innerHTML = "Please enter email ";
+    messege.style.color = "red";
+    email.focus();
+  } else if (password.value === "") {
+    messege.innerHTML = "Please enter password";
+    messege.style.color = "red";
+    password.focus();
+  } else {
+    let userdata = {
+      email: email.value,
+      password: password.value,
+    };
+console.log(userdata)
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(userdata.email, userdata.password)
+      .then((resolve) => {
+        messege.innerHTML = "Signup succsesfully ";
+        messege.style.color = "green";
+        console.log(resolve.userdata);
+      })
+      .catch((error) => {
+        messege.innerHTML = error.messege;
+        messege.style.color = "red";
+      });
+  }
+}
+function passbtn() {
+  let password = document.getElementById("password");
+  let passbtn=document.getElementById("showwbtn");
+  if(password.type==="password"){
+    password.type="text";
+    passbtn.innerHTML="Hide"
+  }else{
+    password.type="password";
+    passbtn.innerHTML="Show"
+  }
+}
